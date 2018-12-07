cdfd30abcab1fa314f9d9e1e0fa125921a726a68
Jona Cwejman <jona@bobby.se>
2018-12-07 14:06:31 +0100

    Add contact section

---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import Header from './Header';
+
+const Contact = () => [
+  <Header id="Contact" key="header-conctact" title="KONTAKT" src="/img/wide2.jpg" />,
+  <div className="Contact" key="contact">
+    <p className="Contact-line">031 55 06 62</p>
+    <p className="Contact-line">Skanstorget 10, 411 22 Göteborg</p>
+  </div>,
+];
+
+export default Contact;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,16 @@ import { graphql } from 'gatsby';
 import { cold } from 'react-hot-loader';
 import * as R from 'ramda';
 
-import { Nav, Menu, Cover, Intro, Booking } from '../components';
+import { Nav, Menu, Cover, Intro, Booking, Contact } from '../components';
 
 const frontmatterPath = ['data', 'markdownRemark', 'frontmatter'];
 
 const toTemplate = sections => data => R.map(R.applyTo(data), sections);
 
 export const Template = () => <div>Preview can not be enabled at this time</div>
-export const Site = toTemplate([Nav, Cover, Intro, Menu, Booking]);
+export const Site = toTemplate([Nav, Cover, Intro, Menu, Booking, Contact]);
 
-export default cold(R.compose(Site, x => { console.log(x); return x }, R.path(frontmatterPath)));
+export default cold(R.compose(Site, R.path(frontmatterPath)));
 
 // Gatsby AST GQL
 
