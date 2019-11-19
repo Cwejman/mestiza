878ecbd11e1db9451d82d9dff2770f27013f4fc7
Jona <jcwejman@gmail.com>
2019-11-19 15:02:29 +0100

    Feat: Remove booking form in favor of 3rd party

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -1,99 +1,17 @@
-import moment from 'moment';
 import * as React from 'react';
-import * as R from 'ramda';
 
 import Header from './Header';
 
-const toEventVal = R.path(['target', 'value']);
+const bookingLink = 'https://module.lafourchette.com/sv_SE/module/566299-f811d';
 
-const useFormInput = (trafo = x => x, init) => {
-  const [value, setValue] = React.useState(trafo(init));
-
-  return {
-    value,
-    onChange: R.compose(setValue, toEventVal),
-    onBlur: () => setValue(trafo(value)),
-  };
-};
-
-const Input = ({ label, ...rest }) => (
-  <div className={`Booking-group ${label}`}>
-    <p className="Booking-label">{label}</p>
-    <input {...rest} />
-  </div>
-);
-
-
-const toInterpolDate = (minIsoDay, maxIsoDay) => date => {
-  const now = moment();
-  const input = moment(date);
-  const next = now.isAfter(input) ? now : input;
-
-  if (next.isoWeekday() < minIsoDay) return next.isoWeekday(minIsoDay).format('YYYY-MM-DD');
-  if (next.isoWeekday() > maxIsoDay) return next.add(1, 'week').isoWeekday(minIsoDay).format('YYYY-MM-DD');
-  return next.format('YYYY-MM-DD');
-};
-
-const roundMoment = (input) => {
-  const rounded = Math.round(input.minute() / 15) * 15;
-  return input.minute(rounded)
-};
-
-const hourToDate = hour => `${hour}:00`;
-
-const toInterpolTime = (minHour, maxHour) => (time) => {
-  const min = moment(minHour, 'HH');
-  const max = moment(maxHour, 'HH');
-  const input = roundMoment(moment(time, 'HH:mm'));
-
-  if (input.isBefore(min)) return min.format('HH:mm');
-  if (input.isAfter(max)) return max.format('HH:mm');
-  return input.format('HH:mm');
-};
-
-const dayMin = 2;
-const dayMax = 6;
-const hourMin = 16;
-const hourMax = 20;
-
-const Booking = () => {
-  const interpolDate = toInterpolDate(dayMin, dayMax);
-  const interpolTime = toInterpolTime(hourMin, hourMax);
-
-  const timeMin = hourToDate(hourMin);
-  const timeMax = hourToDate(hourMax);
-
-  const date = useFormInput(interpolDate, Date.now());
-  const time = useFormInput(interpolTime, Date.now());
-  const amount = useFormInput();
-  const phone = useFormInput();
-  const name = useFormInput();
-  const mail = useFormInput();
-
-  return [
-    <Header id="Booking" key="header-booking" title="BOKA BORD" src="/img/wide2.jpg" />,
-    <div className="Booking" key="booking">
-      <form className="Booking-form" name="booking" method="post" data-netlify-honeypot="4bots" data-netlify="true">
-        <p style={{ display: 'none' }}>
-          <input name="4bots" />
-          <input type="hidden" name="form-name" value="booking" />
-        </p>
-        <div className="Booking-row">
-          <Input {...date} name="date" label="Datum" type="date" />
-          <Input {...time} min={timeMin} max={timeMax} name="time" label="Tid" type="time" />
-        </div>
-        <div className="Booking-row">
-          <Input {...name} name="name" label="För och efternamn" type="text" />
-          <Input {...amount} name="people" label="Antal" type="number" min="1" max="6" />
-        </div>
-        <div className="Booking-row">
-          <Input {...mail} name="mail" label="Mejl" type="email" />
-          <Input {...phone} name="phone" label="Telefon-nr" type="tel" />
-        </div>
-        <button type="submit">Skicka bokningsförfrågan</button>
-      </form>
-    </div>,
-  ];
-};
+const Booking = () => [
+  <Header id="Booking" key="header-booking" title="BOKA BORD" src="/img/wide2.jpg" />,
+  <div className="Booking" key="booking">
+    {/* <script type="text/javascript" src="https://module.lafourchette.com/sv_SE/js/horizontal/566299-f811d" /> */}
+    {/* <script type="text/javascript" src="https://module.lafourchette.com/sv_SE/js/vertical/566299-f811d" /> */}
+    {/* <script type="text/javascript" src="https://module.lafourchette.com/sv_SE/js/button/566299-f811d" /> */}
+    <button onClick={() => window.open(bookingLink)}>Gå till bokningtjänst</button>
+  </div>,
+];
 
 export default Booking;
