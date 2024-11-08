95d3997a9dc8fa6ffd99986394522f089f9e69b2
cwejman <jcwejman@gmail.com>
2024-11-08 11:05:40 +0100

    feat: Remove brunch/catering menu + refactor
    The experimentation I did with ramda and react was really quite compelx to the eyes and see no bennefit what so ever in doing so. Simplified it and removed ramda from the menu.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,92 +1,59 @@
-import React from 'react';
-import { v4 } from 'uuid';
-import * as R from 'ramda';
+import React, { Fragment } from 'react';
 
-import * as H from '../hooks';
-import * as U from '../utils';
 import Header from './Header';
 
-// Components
+//
 
-const Menus = ({ catering, normal }) => {
-  const [onCatering, toogleOnCatering] = H.useBool()
-
-  return [
-    <Header key="header-menu" id="Menu" title="MENY" src="/img/plate3.jpg" />,
-    <Sections key="sections">
-      <CateringSwitch onClick={toogleOnCatering} value={onCatering} />
-      {onCatering ? catering : normal}
-    </Sections>,
-  ];
-};
-
-const CateringSwitch = ({ value, onClick }) => (
-  <div className="Menus-section-cateringSwitch">
-    <div className="Menus-cateringSwitch" onClick={onClick}>
-      <div className={`Menus-cateringSwitch-el${value ? ' off' : ' on'}`}>Kvällsmeny</div>
-      <div className={`Menus-cateringSwitch-el${value ? ' on' : ' off'}`}>Brunchmeny</div>
+export default ({ menus }) => (
+  <>
+    <Header id="Menu" title="MENY" src="/img/plate3.jpg"/>,
+    <div className="Menus">
+      {menus.map(({ dishes }, i) =>
+        <Fragment key={i}>
+          {i !== 0 && <p className="Menus-seperator">=</p>}
+          <Menu dishes={dishes} key={i}/>
+        </Fragment>
+      )}
     </div>
-  </div>
+  </>
 );
 
-const Sections = ({ children }) => (
-  <div className="Menus" key="menus">
-    {children}
-  </div>
-);
+//
 
-const Section = ({ children }) => (
-  <div className="Menus-section" key={`menu${v4()}`}>
-    {children}
-  </div>
-);
+const parseAlternatives = (list) => list
+  .filter(alt => alt.name !== 'PLACEHOLDER')
+  .map(alt => ({ ...alt, isAlternative: true }));
 
-const Dish = ({ price, name, isAlternative }) => (
-  <div className={`Menus-dish${isAlternative ? ' alt' : ''}`} key={name}>
-    {!isAlternative && <p className="Menus-dish-bullet">*</p>}
-    <p className={`Menus-dish-name${isAlternative ? ' alt' : ''}`}>{name}</p>
-    {price !== '123456' && price && (
-      <p className="Menus-dish-price">
-        {`${price}$`}
-      </p>
-    )}
-  </div>
-);
+function Menu ({ dishes }) {
+  const dishesAndAlternatives = dishes.reduce(
+    (acc, { alternatives, ...dish }) => {
+      console.log({ dish, alternatives, parsedAlt: parseAlternatives(alternatives) });
+      return [...acc, dish, ...parseAlternatives(alternatives)];
+    },
+    [],
+  );
 
-const Seperator = () => (
-  <p className="Menus-seperator" key={`seperator${v4()}`}>=</p>
-);
+  return (
+    <div className="Menus-section">
+      {dishesAndAlternatives.map((el, i) => (
+        <Dish key={i} {...el} />
+      ))}
+    </div>
+  );
+}
 
-// Composition: Export
+//
 
-const toAlternative = R.assoc('isAlternative', true);
-const ejectAlternatives = (xs, x) => R.concat(
-  [...xs, x],
-  R.map(toAlternative, x.alternatives || []),
-);
-
-const alternativesPlaceholder = [{ name: 'PLACEHOLDER', price: '123456' }]
-
-const selectSectionChildren = R.compose(
-  R.map(Dish),
-  R.reduce(ejectAlternatives, []),
-  R.map(R.evolve({
-    alternatives: R.when(R.equals(alternativesPlaceholder), x => []),
-  })),
-  R.prop('dishes'),
-);
-
-const toSection = R.compose(Section, U.toChildren, selectSectionChildren);
-
-const toMenuChildren = R.compose(
-  U.intersperseComponent(Seperator),
-  R.map(toSection),
-);
-
-const toMenuProps = R.applySpec({
-  normal: R.pipe(R.prop('menus'), toMenuChildren),
-  catering: R.pipe(R.prop('menusCatering'), toMenuChildren),
-});
-const toMenus = R.compose(Menus, toMenuProps);
-
-export default toMenus;
+function Dish ({ price, name, isAlternative }) {
+  return (
+    <div className={`Menus-dish${isAlternative ? ' alt' : ''}`} key={name}>
+      {!isAlternative && <p className="Menus-dish-bullet">*</p>}
+      <p className={`Menus-dish-name${isAlternative ? ' alt' : ''}`}>{name}</p>
+      {price !== '123456' && price && (
+        <p className="Menus-dish-price">
+          {`${price}$`}
+        </p>
+      )}
+    </div>
+  );
+}
